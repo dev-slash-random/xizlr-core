477dc8b3c996954e1a6a0a66df00092b88ac5d8c
Ken Lalobo <lalobo@gmail.com>
2016-02-16 16:36:20 +0000

    minor bug fixes

--- src/AbstractController.php
+++ src/AbstractController.php
@@ -7,17 +7,18 @@
 
 namespace Mooti\Xizlr\Core;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractController
 {
-	abstract public function getAll();
+	abstract public function getAll(Request $request, Response $response);
 
-	public function render($content, Response $response)
+	public function render(array $content, Response $response)
 	{
 		$response->setStatusCode(Response::HTTP_OK);
 		$response->headers->set('Content-Type', 'text/json');
-		$response->setContent(json_encode($content->format()));
+		$response->setContent(json_encode($content));
 
 		return $response;
 	}
--- src/RestApplication.php
+++ src/RestApplication.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use League\Route\RouteCollection;
 
-class WebApplication
+class RestApplication
 {
     use Xizlr;
 
